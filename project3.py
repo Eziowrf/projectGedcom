77774c20da819865c9074afcdd87294f1c2711bf
Eziowrf <42254101+Eziowrf@users.noreply.github.com>
2018-09-30 15:58:40 -0400

    Update project3.py
    remove the age and format function outside

--- project3.py
+++ project3.py
@@ -1,37 +1,8 @@
 #opening file
 from prettytable import PrettyTable
 import time
-def FormatDate(date):
-    def monthToNum(month):
-        numbers = {
-            "JAN": "01",
-            "FEB": "02",
-            "MAR": "03",
-            "APR": "04",
-            "MAY": "05",
-            "JUN": "06",
-            "JUL": "07",
-            "AUG": "08",
-            "SEP": "09",
-            "OCT": "10",
-            "NOV": "11",
-            "DEC": "12",
-        }
-        return numbers.get(month, None)
-    if date != "NA":
-        date_list = date.split(' ')
-        return date_list[2] + '-' + monthToNum(date_list[1]) + '-' + date_list[0].zfill(2)
-    else:
-        return "NA"
-
-def computeAge(Fdate):
-    timeList = Fdate.split('-')
-    currentTimeList = time.strftime('%Y-%m-%d', time.localtime(time.time())).split('-')
-    tempAge = int(currentTimeList[0]) - int(timeList[0])
-    if int(currentTimeList[1]+ currentTimeList[2]) - int(timeList[1]+ timeList[2]) >= 0:
-        return tempAge
-    else:
-        return tempAge - 1
+from FormatDate import FormatDate
+from computeAge import computeAge
 ged_file=open(r'family.ged')
 class Person:
     ID = "NA"
