bd37ccddd481b9b709d7f2d0b9685297fe00b076
Eziowrf <42254101+Eziowrf@users.noreply.github.com>
2018-10-12 14:26:21 -0400

    package all function in one gedcom file

--- Sprint1/test_output_sprint1.py
+++ Sprint1/test_output_sprint1.py
@@ -1,31 +1,33 @@
+import userStory01 
+import userStory02 
+import userStory09 
+import userStory10 
+import userStory17 
+import userStory18 
+import userStory22
+import userStory25
+import userStory26
 import project3
-import userStory01
-import userStory02
-import userStory09
-import userStory10
-import userStory17
-import userStory18
 
-(listOfPeople,listOfFamilies)= project3.gedComParse('US_01_02_test.ged')
+(listOfPeople,listOfFamilies)= project3.gedComParse('error.ged')
 project3.present(listOfPeople,listOfFamilies)
+(listOfPeople1,listOfFamilies1)= project3.gedComParse('correct.ged')
+project3.present(listOfPeople1,listOfFamilies1)
+
 userStory01.userStory01(listOfPeople, listOfFamilies)
-
-(listOfPeople,listOfFamilies)= project3.gedComParse('US_01_02_test.ged')
-project3.present(listOfPeople,listOfFamilies)
 userStory02.userStory02(listOfPeople, listOfFamilies)
-
-(listOfPeople,listOfFamilies)= project3.gedComParse('US_09_test.ged')
-project3.present(listOfPeople,listOfFamilies)
 userStory09.us_09(listOfFamilies, listOfPeople)
-
-(listOfPeople,listOfFamilies)= project3.gedComParse('US_10_test.ged')
-project3.present(listOfPeople,listOfFamilies)
 userStory10.us_10(listOfFamilies, listOfPeople)
-
-(listOfPeople,listOfFamilies)= project3.gedComParse('US_17_test.ged')
-project3.present(listOfPeople,listOfFamilies)
-userStory17.US_17(listOfPeople)
-
-(listOfPeople,listOfFamilies)= project3.gedComParse('US_18_test.ged')
-project3.present(listOfPeople,listOfFamilies)
+userStory17.US_17(listOfPeople,listOfFamilies)
 userStory18.US_18(listOfPeople)
+userStory25.userStory25(listOfPeople,listOfFamilies)
+userStory26.us_26(listOfFamilies,listOfPeople)
+
+userStory01.userStory01(listOfPeople1, listOfFamilies1)
+userStory02.userStory02(listOfPeople1, listOfFamilies1)
+userStory09.us_09(listOfFamilies1, listOfPeople1)
+userStory10.us_10(listOfFamilies1, listOfPeople1)
+userStory17.US_17(listOfPeople1,listOfFamilies1)
+userStory18.US_18(listOfPeople1)
+userStory25.userStory25(listOfPeople1,listOfFamilies1)
+userStory26.us_26(listOfFamilies1,listOfPeople1)
