4fad857502d95e990d3b4234eee23d9c438c8048
MarkWatson95 <markle951@yahoo.com>
2018-10-24 22:17:57 -0400

    fixed small problem with us 27

--- userStory27.py
+++ userStory27.py
@@ -1,11 +1,10 @@
 #Made by Mark Watson
 #Fixes age for peoples whom have died
 
-from addAge import addAge
 from computeAge import computeAge
 def us_27(listOfPeople, listOfFamilies):
     
 
     for people in listOfPeople:
-        if (Alive == False):
+        if (people.Alive == False):
             people.Age = people.Age - computeAge(people.Death)
