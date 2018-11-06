bf48982b24526385ec83609b56db65830158e6f8
Eziowrf <eziocheng@gmail.com>
2018-11-06 17:28:45 -0500

    add US21 and US22

--- test_output_sprint3.py
+++ test_output_sprint3.py
@@ -17,8 +17,8 @@ import userStory17
 import userStory18
 import userStory19
 import userStory20
-#import userStory21
-#import userStory22
+import userStory21
+import userStory22
 
 import userStory25
 import userStory26
@@ -27,36 +27,40 @@ import userStory28
 #import userStory29
 #import userStory30
 
-(listOfPeople,listOfFamilies)= main.gedComParse('error3.ged')
-userStory27.us_27(listOfPeople)
-main.present(listOfPeople,listOfFamilies)
-# (listOfPeople1,listOfFamilies1)= project3.gedComParse('correct.ged')
-# project3.present(listOfPeople1,listOfFamilies1)
+(listOfPeople,listOfFamilies)= main.gedComParse('correct2.ged')
+(listOfPeople2,listOfFamilies2)= main.gedComParse('correct2.ged')
+(listOfPeople3,listOfFamilies3)= main.gedComParse('error3.ged')
+
+userStory27.us_27(listOfPeople3)
+main.present(listOfPeople3,listOfFamilies3)
+
 
 userStory01.userStory01(listOfPeople, listOfFamilies)
 userStory02.userStory02(listOfPeople, listOfFamilies)
-userStory03.userStory03(listOfPeople)
-userStory04.userStory04(listOfFamilies)
-userStory05.userStory05(listOfPeople, listOfFamilies)
-userStory06.userStory06(listOfPeople, listOfFamilies)
-
 userStory09.us_09(listOfFamilies, listOfPeople)
 userStory10.us_10(listOfFamilies, listOfPeople)
-userStory11.us_11(listOfFamilies)
-userStory12.us_12(listOfFamilies, listOfPeople)
-userStory13.us_13(listOfFamilies, listOfPeople)
-userStory14.us_14(listOfFamilies, listOfPeople)
-
 userStory17.US_17(listOfPeople,listOfFamilies)
 userStory18.US_18(listOfPeople)
-userStory19.us_19(listOfPeople, listOfFamilies)
-userStory20.us_20(listOfPeople, listOfFamilies)
-#userStory21.us_21(listOfPeople, listOfFamilies)
-#userStory22.us_22(listOfPeople, listOfFamilies)
-
 userStory25.userStory25(listOfPeople,listOfFamilies)
 userStory26.us_26(listOfFamilies,listOfPeople)
-userStory28.us_28(listOfPeople, listOfFamilies)
+
+
+
+userStory03.userStory03(listOfPeople2)
+userStory04.userStory04(listOfFamilies2)
+userStory11.us_11(listOfFamilies2)
+userStory12.us_12(listOfFamilies2, listOfPeople2)
+userStory19.us_19(listOfPeople2, listOfFamilies2)
+userStory20.us_20(listOfPeople2, listOfFamilies2)
+#userStory28.us_28(listOfPeople2, listOfFamilies2)
+
+
+userStory05.userStory05(listOfPeople3, listOfFamilies3)
+userStory06.userStory06(listOfPeople3, listOfFamilies3)
+userStory13.us_13(listOfFamilies3, listOfPeople3)
+userStory14.us_14(listOfFamilies3, listOfPeople3)
+userStory21.us_21(listOfPeople3, listOfFamilies3)
+userStory22.us_22(listOfPeople3, listOfFamilies3)
 #userStory29.us_29(listOfPeople)
 #userStory30.us_30(listOfPeople, listOfFamilies)
 
