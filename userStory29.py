200ece58ffefdc80b78f694c0f63ee993dc3a3b8
MarkWatson95 <markle951@yahoo.com>
2018-11-07 17:30:36 -0500

    completed user stories 29 and 30

---
+++ userStory29.py
@@ -0,0 +1,11 @@
+#Made by Mark Watson
+
+#prints a list of dead people
+def us_29(listOfPeople, listOfFamilies):
+
+    deathList = {}
+    for people in listOfPeople:
+        if people.Alive != True:
+            deathList.append(people.Name)
+
+    print(deathList)
---
+++ userStory30.py
@@ -0,0 +1,13 @@
+#Made by Mark Watson
+
+#Prints a list of married alive people
+def us_30(listOfPeople, listOfFamilies):
+
+    marriedList = {}
+
+    for people in listOfPeople:
+        if people.Spouse != "NA" and people.Alive == True:
+
+            marriedList.append(people.Name)
+
+    print(marriedList)
